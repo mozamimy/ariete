1cc532ae41b81f5525368deb18790983c715f73c
Moza USANE <mozamimy@quellencode.org>
2014-11-11 16:25:46 +0900

    Rewrited test codes from test/unit to rspec.

---
+++ spec/ariete_spec.rb
@@ -0,0 +1,20 @@
+require "spec_helper"
+
+RSpec.describe Ariete do
+  include Ariete
+
+  def output
+    puts "Ariete is a kind of rabbit."
+    warn "Ariete means 'Lop' in Italian."
+  end
+
+  describe "#capture_stdout" do
+    subject { capture_stdout { output } }
+    it { expect(subject).to eq "Ariete is a kind of rabbit.\n" }
+  end
+
+  describe "#capture_stderr" do
+    subject { capture_stderr { output } }
+    it { expect(subject).to eq "Ariete means 'Lop' in Italian.\n" }
+  end
+end
