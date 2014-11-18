5d2dca626391e95a71079030288bfc5b459fd727
Moza USANE <mozamimy@quellencode.org>
2014-11-18 17:44:11 +0900

    Added a feature that is custom matchers of RSpec.

--- lib/ariete.rb
+++ lib/ariete.rb
@@ -24,3 +24,15 @@ module Ariete
     module_function "capture_#{name}"
   end
 end
+
+RSpec::Matchers.define :be_output do |expect|
+  match do |actual|
+    Ariete.capture_stdout { actual.call } == expect.to_s
+  end
+end
+
+RSpec::Matchers.define :be_output_to_stderr do |expect|
+  match do |actual|
+    Ariete.capture_stderr { actual.call } == expect.to_s
+  end
+end
