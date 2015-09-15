2a896060bd39c8df876a6d7b1537060dea4e4f47
stk132 <stk0724@gmail.com>
2015-09-16 02:50:21 +0900

    fix rspec dependency probrem that cause NameSpaceError in case of using test-unit.

--- lib/ariete.rb
+++ lib/ariete.rb
@@ -2,7 +2,7 @@
 # Copyright (c) 2014 Moza USANE
 # This software is released under the MIT License.
 # http://opensource.org/licenses/mit-license.php
-
+require "rspec"
 require "stringio"
 
 module Ariete
